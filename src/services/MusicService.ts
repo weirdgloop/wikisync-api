008e67515331639d77e8a85a0e044f508d424dcf
Allen Kinzalow <allen.kinzalow@gmail.com>
2023-09-06 23:16:40 -0400

    Updated music track scripts & music service

--- src/services/MusicService.ts
+++ src/services/MusicService.ts
@@ -1,20 +1,19 @@
 import { isBitSet } from '../util/util';
-import MUSIC_TRACK_VARPS from '../data/musicVarps.json'
-import MUSIC_TRACKS from '../data/musicTracks.json'
+import MUSIC_TRACK_VARPS from '../data/musicVarps.json';
+import MUSIC_TRACKS from '../data/musicTracks.json';
 
 class MusicService {
   // https://github.com/Joshua-F/cs2-scripts/blob/master/scripts/%5Bclientscript%2Cmusic_init_counter%5D.cs2
   public static async getMusicTracks(data) {
     if (data.varps[MUSIC_TRACK_VARPS[0]] === undefined) {
       // If the user doesn't have the most basic varp set, don't return any data
-      return null
+      return null;
     }
 
     const results = [];
 
-    MUSIC_TRACKS.forEach(val => {
-      const varpBitIndex = val.varpCoord % 16384;
-      const varpIndex = Math.floor(val.varpCoord / 16384);
+    MUSIC_TRACKS.forEach((val) => {
+      const { varpBitIndex, varpIndex } = val;
       if (varpIndex === -1) {
         // This track is automatically unlocked
         results.push(val.trackId);
@@ -24,14 +23,14 @@ class MusicService {
       const varp = data.varps[varpId.toString()];
       if (isBitSet(varp, varpBitIndex)) {
         // This track is unlocked the normal way
-        results.push(val.trackId)
+        results.push(val.trackId);
       } else if (val.trackId === 331 && data.varbs['1028'] > 35) {
         // Very weird case: a singular track from A Tail of Two Cats can be unlocked via quest varbit
-        results.push(val.trackId)
+        results.push(val.trackId);
       }
     });
     return results;
   }
 }
 
-export { MusicService, MUSIC_TRACK_VARPS }
+export { MusicService, MUSIC_TRACK_VARPS };
