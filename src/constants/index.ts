80de9b59e27079e1c46cdb8078806c48c507c66c
andmcadams <andmcadams@gmail.com>
2025-02-02 13:02:18 -0500

    I hate prettier

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -12,64 +12,44 @@ export const MANIFEST_VERSION = 1;
 /**
  * The varbits that we require from RuneLite.
  */
-export const REQUIRED_VARBITS = [
-  ...QUEST_VARBITS,
-  ...DIARY_VARBITS,
-  3637,
-  3202,
-  3207,
-  3208,
-  5358,
-  10663,
-  10664,
-  10665,
-  10666,
+export const REQUIRED_VARBITS = [...QUEST_VARBITS, ...DIARY_VARBITS, 3637, 3202, 3207, 3208, 5358, 10663, 10664, 10665, 10666,
   // 13784 used to be required by the quest Architectural Alliance.
   // We tried removing this varbit, but it caused the RL plugin to send empty updates every 10 seconds.
   // For now, we will continue to ask for this varbit. We can remove this varbit once the RL plugin is updated to handle varbit removal.
-  13784,
-  9653,
-  9655,
-];
+  13784, 9653, 9655, ];
 
 /**
  * The varps that we require from RuneLite.
  */
-export const REQUIRED_VARPS = [
-  ...DIARY_VARPS,
-  ...LEAGUE_TASK_VARPS,
-  ...QUEST_VARPS,
-  ...COMBAT_ACHIEVEMENTS_VARPS,
-  ...MUSIC_TRACK_VARPS,
-];
+export const REQUIRED_VARPS = [...DIARY_VARPS, ...LEAGUE_TASK_VARPS, ...QUEST_VARPS, ...COMBAT_ACHIEVEMENTS_VARPS, ...MUSIC_TRACK_VARPS];
 
 /**
  * Valid skills to save in the database.
  */
 export const SKILL_NAMES = [
-  "Attack",
-  "Strength",
-  "Defence",
-  "Ranged",
-  "Prayer",
-  "Magic",
-  "Runecraft",
-  "Hitpoints",
-  "Crafting",
-  "Mining",
-  "Smithing",
-  "Fishing",
-  "Cooking",
-  "Firemaking",
-  "Woodcutting",
-  "Agility",
-  "Herblore",
-  "Thieving",
-  "Fletching",
-  "Slayer",
-  "Farming",
-  "Construction",
-  "Hunter",
+  'Attack',
+  'Strength',
+  'Defence',
+  'Ranged',
+  'Prayer',
+  'Magic',
+  'Runecraft',
+  'Hitpoints',
+  'Crafting',
+  'Mining',
+  'Smithing',
+  'Fishing',
+  'Cooking',
+  'Firemaking',
+  'Woodcutting',
+  'Agility',
+  'Herblore',
+  'Thieving',
+  'Fletching',
+  'Slayer',
+  'Farming',
+  'Construction',
+  'Hunter',
 ];
 
 /**
