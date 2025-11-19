424c56b5016db1b43c33d5f2b94f56af73c9248c
Zander B <lezed1@gmail.com>
2025-11-18 20:45:30 -0500

    Sailing!

---
+++ src/runelite/transformers/SailingTransformer.ts
@@ -0,0 +1,25 @@
+import { isBitSet } from '../../util/util';
+import SAILING_CHART_VARPS from '../data/sailingChartVarps.json';
+
+class SailingTransformer {
+  public static async getSailingData(data) {
+    if (data.varps[SAILING_CHART_VARPS[0]] === undefined) {
+      // If the user doesn't have the most basic varp set, don't return any data
+      return null;
+    }
+
+    const results = [];
+
+    SAILING_CHART_VARPS.forEach((val, index) => {
+      const varp = data.varps[val.toString()];
+      for (let i = 0; i < 32; i++) {
+        if (isBitSet(varp, i)) {
+          results.push(32 * index + i);
+        }
+      }
+    });
+    return results;
+  }
+}
+
+export { SailingTransformer, SAILING_CHART_VARPS };
